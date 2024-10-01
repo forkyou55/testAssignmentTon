3d29ad766d33515b65e36fd72e918a4207bc62c6
stanislavivanishchev <ivanishevstanislav@gmail.com>
2024-10-01 16:44:30 +0200

    fix export

--- src/fixtures/rewardData.js
+++ src/fixtures/rewardData.js
@@ -1,34 +1,36 @@
 const rewardData = {
-    "item_metadata": {
-        "buttons": [
-            {
-                "uri": "https://society.ton.org/welcome",
-                "label": "Open in TON Society!"
-            }
-        ],
-        "attributes": [
-            {
-                "value": "TON Society",
-                "trait_type": "Organizer"
-            },
-            {
-                "trait_type": "Date",
-                "value": "22 Mar 2024"
-            }
-        ],
-        "activity_type": "Event",
-        "organizer": "TON Society",
-        "original_activity_url": "https://lu.ma/348yfd",
-        "start_date_iso": "2024-03-22",
-        "end_date_iso": "2024-03-22",
-        "place": {
-            "type": "Online",
-            "country_code_iso": "HK",
-            "place_coordinates_osm": "22.2793278,114.1628132",
-            "venue_name": "TC"
-        },
-        "user_data": "user message!"
+  "item_metadata": {
+    "buttons": [
+      {
+        "uri": "https://society.ton.org/welcome",
+        "label": "Open in TON Society!"
+      }
+    ],
+    "attributes": [
+      {
+        "value": "TON Society",
+        "trait_type": "Organizer"
+      },
+      {
+        "trait_type": "Date",
+        "value": "22 Mar 2024"
+      }
+    ],
+    "activity_type": "Event",
+    "organizer": "TON Society",
+    "original_activity_url": "https://lu.ma/348yfd",
+    "start_date_iso": "2024-03-22",
+    "end_date_iso": "2024-03-22",
+    "place": {
+      "type": "Online",
+      "country_code_iso": "HK",
+      "place_coordinates_osm": "22.2793278,114.1628132",
+      "venue_name": "TC"
     },
-    "telegram_user_id": 178210051,
-    "wallet_address": "UQC8q7sFEuDrNiZPiA--NudnByGfl3fyqANb7SECQnibYViZ"
-};  
+    "user_data": "user message!"
+  },
+  "telegram_user_id": 178210051,
+  "wallet_address": "UQC8q7sFEuDrNiZPiA--NudnByGfl3fyqANb7SECQnibYViZ"
+};
+
+export default rewardData;
